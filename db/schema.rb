5799143c8386d26edb9ce5dd784c5147084eb30b
soni17 <elb004@gmail.com>
2018-01-03 03:44:26 -0500

    db migration: rename party guests table to widgets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180103080717) do
+ActiveRecord::Schema.define(version: 20180103083951) do
 
   create_table "locations", force: :cascade do |t|
     t.string "name"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20180103080717) do
     t.integer "quantity"
   end
 
-  create_table "party_guests", force: :cascade do |t|
+  create_table "widgets", force: :cascade do |t|
     t.string  "first_name"
     t.string  "last_name"
     t.string  "dietary_restrictions"
